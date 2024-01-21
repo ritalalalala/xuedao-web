ce21306db0e59c417e33bde9f2f97bae65c225e2
kevinsslin <kk123750964@gmail.com>
2024-01-21 18:47:08 +0800

    chore: wrap Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import Link from "next/link";
 
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 import "./globals.css";
@@ -28,25 +28,7 @@ export default function RootLayout({
         <div className="flex min-h-screen flex-col py-12">
           <Header />
           {children}
-          <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6">
-            <p className="text-xs text-gray-500 dark:text-gray-400">
-              © 2024 XueDAO. All rights reserved.
-            </p>
-            <nav className="flex gap-4 sm:ml-auto sm:gap-6">
-              <Link
-                className="text-xs underline-offset-4 hover:underline"
-                href="#"
-              >
-                Terms of Service
-              </Link>
-              <Link
-                className="text-xs underline-offset-4 hover:underline"
-                href="#"
-              >
-                Privacy
-              </Link>
-            </nav>
-          </footer>
+          <Footer />
         </div>
       </body>
     </html>
