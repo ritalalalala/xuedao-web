1fbc476377e8c230c0902a80e14f38bbb5e13374
kevinsslin <kk123750964@gmail.com>
2024-02-15 01:41:17 +0800

    chore: update logo size in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <div className="sticky top-0 z-10 flex h-20 items-center gap-2 bg-white px-4 lg:px-8">
       <Link className="mr-auto flex items-center" href="/">
-        <Image src="/logo.png" alt="Xue DAO logo" width={150} height={150} />
+        <Image src="/logo.png" alt="Xue DAO logo" width={200} height={200} />
         <span className="sr-only">XueDAO</span>
       </Link>
       <nav className="ml-auto flex flex-row gap-4 text-base font-medium sm:flex-row sm:gap-6 sm:text-xl">
